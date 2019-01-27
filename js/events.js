08867cf99c4d74f3e869abaddbc15b94c268810a
Rakan Shaker <shaker.rakan@gmail.com>
2019-01-27 23:53:51 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,21 @@
 
 $(document).ready(function(){
 
-// call functions here
+$('p').on('click', function getIt(){
+   alert('Hey!');
+}); 
+
+$('img').on('load', function frameIt(){
+  $('img').addClass('tasty');
+});
+$(document).on('keydown', function pressIt(){
+  if ($('input:first').val()==='g'){
+    alert('you pressed g!');
+  }
+});
+$('form').on('submit', function submitIt(){
+  alert('Your form is going to be submitted now.');
+});
 
 });
+ 
